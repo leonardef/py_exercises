7c557749e548b63fa47c0de8539df03dc72f45d9
Leonarde <leonardenf@gmail.com>
2019-09-24 22:09:42 -0300

    Correção, números em um só dicionário.

--- q3.py
+++ q3.py
@@ -9,10 +9,10 @@
 # {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
 
 num = int(input("Número: "))
+dic = {}
 
 
 for numeros in range(1, num + 1):
-    dici = {
-        numeros: numeros * numeros
-    }
-    print(dici, end=", ")
+    dic[numeros] = numeros * numeros 
+
+print(dic)
